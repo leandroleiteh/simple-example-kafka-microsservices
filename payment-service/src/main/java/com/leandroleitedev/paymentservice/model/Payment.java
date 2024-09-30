798b8f8528b932cbe75dd422a185c0da6c60d7ac
leandro.henrique <leandroluz201616@gmail.com>
2024-09-29 23:08:23 -0300

    feat: add producer config

--- payment-service/src/main/java/com/leandroleitedev/paymentservice/model/Payment.java
+++ payment-service/src/main/java/com/leandroleitedev/paymentservice/model/Payment.java
@@ -1,9 +1,11 @@
 package com.leandroleitedev.paymentservice.model;
 
 import lombok.Getter;
+import lombok.ToString;
 
 import java.io.Serializable;
 
+@ToString
 @Getter
 public class Payment implements Serializable {
 
--- payment-service/src/main/java/com/leandroleitedev/paymentservice/services/impl/PaymentServiceImpl.java
+++ payment-service/src/main/java/com/leandroleitedev/paymentservice/services/impl/PaymentServiceImpl.java
@@ -2,15 +2,29 @@ package com.leandroleitedev.paymentservice.services.impl;
 
 import com.leandroleitedev.paymentservice.model.Payment;
 import com.leandroleitedev.paymentservice.services.PaymentService;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+import java.io.Serializable;
+
+@RequiredArgsConstructor
 @Service
 @Slf4j
 public class PaymentServiceImpl implements PaymentService {
 
+    private final KafkaTemplate<String, Serializable> kafkaTemplate;
+
+    @SneakyThrows
     @Override
     public void payment(Payment payment) {
-        log.info(" PAYMENT_SERVICE_IMPL ::: Payment sent: {}", payment);
+        log.info("PAYMENT RECEIVE: {} ", payment);
+        Thread.sleep(2000);
+
+        log.info("PAYMENT SEND: {} ", payment);
+
+        kafkaTemplate.send("payment-topic", payment);
     }
 }
